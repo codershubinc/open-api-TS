cf1dd1ada3c349553087be96914d33321930f13a
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-02-16 00:08:10 +0530

    Add Express.js as a dependency and set up a basic server

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express'
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
+
