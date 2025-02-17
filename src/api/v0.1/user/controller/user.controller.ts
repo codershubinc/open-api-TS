782237f835bd69177d3b767bdff931c4fe0d3ac7
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-02-17 22:29:42 +0530

    Refactor user controller to remove unnecessary console log

--- src/api/v0.1/user/controller/user.controller.ts
+++ src/api/v0.1/user/controller/user.controller.ts
@@ -2,13 +2,9 @@ import asynchandler from '@/common/handlers/async.handler';
 import data from '@/data/contryData/utils/handler/contry.data.controller';
 
 const user = asynchandler(async (req, res) => {
+
     const ftData = await data();
-    console.log('ftData', ftData);
-
-
     res.send(ftData);
-
-
 });
 
 export {
