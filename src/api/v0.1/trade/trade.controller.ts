84051a9ddde21dc2982ca8b31e0b1dce5ed5d2d9
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-04-14 19:01:08 +0530

    Refactor trade controller to accept dynamic parameters for symbol and type in getTrade method

--- src/api/v0.1/trade/trade.controller.ts
+++ src/api/v0.1/trade/trade.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { getTradeData } from "./utils/getData";
 
 @Controller('/trade')
 export class TradeController {
     constructor() { }
 
-    @Get('')
-    async getTrade() {
-        const data = getTradeData(
-            'AAPL',
-            'overview',
-        )
-        return data
+    @Get('/:sym/:type')
+    async getTrade(
+        @Param('sym') sym: string,
+        @Param('type') type: string,
+    ) {
+        try {
+            const data = getTradeData(
+                sym,
+                type,
+            )
+            return data
+        } catch (error) {
+            return { error: error.message }
+        }
     }
 }
